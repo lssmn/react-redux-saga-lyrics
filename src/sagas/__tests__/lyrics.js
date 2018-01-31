72431ff2dba604c7c6f2e83a45953ff6d74ce5e6
Alessio Minichiello <alessio.minichiello@LEGO.com>
2018-01-31 00:44:22 +0000

    🎨 Improved tests

--- src/sagas/__tests__/lyrics.js
+++ src/sagas/__tests__/lyrics.js
@@ -8,97 +8,107 @@ import {
 
 describe('Lyrics sagas', () => {
 
-  it('fetchLyrics', () => {
-    const artist = 'Artist';
-    const title = 'Title';
-    const lyrics = 'Lyrics';
+  describe('fetchLyrics', () => {
 
-    const saga = testSaga(fetchLyrics, {
-      type: actions.LYRICS_GET, 
-      payload: {
-        artist,
-        title,
-      }
-    });
-
-    saga
-      .next()
-      .call(service.get, {
-        artist,
-        title,
-      })
-      .next({
-        artist,
-        title,
-        lyrics,
-      })
-      .put({
-        type: actions.LYRICS_GET_SUCCESS,
+    it('successful steps', () => {
+      const artist = 'Artist';
+      const title = 'Title';
+      const lyrics = 'Lyrics';
+  
+      const saga = testSaga(fetchLyrics, {
+        type: actions.LYRICS_GET, 
         payload: {
-          lyrics,
           artist,
           title,
         }
-      })
-      .next()
-      .isDone();
-  });
-
-  it('fetchLyrics error handling', () => {
-    const error = 'Fake';
-    const action = { type: actions.LYRICS_GET };
-    const saga = testSaga(fetchLyrics, action);
-
-    saga
-      .next()
-      .throw(new Error(error))
-      .put({
-        type: actions.LYRICS_GET_FAILED,
-        payload: `Error: ${error}`,
-      })
-      .next()
-      .isDone();
-  });
+      });
   
-  it('fetchSuggestions', () => {
-    const query = 'Query';
-    const suggestions = [];
-
-    const saga = testSaga(fetchSuggestions, {
-      type: actions.LYRICS_SUGGEST, 
-      payload: query,
+      saga
+        .next()
+        .call(service.get, {
+          artist,
+          title,
+        })
+        .next({
+          artist,
+          title,
+          lyrics,
+        })
+        .put({
+          type: actions.LYRICS_GET_SUCCESS,
+          payload: {
+            lyrics,
+            artist,
+            title,
+          }
+        })
+        .next()
+        .isDone();
     });
 
-    saga
-      .next()
-      .call(service.suggest, query)
-      .next({
-        suggestions,
-      })
-      .put({
-        type: actions.LYRICS_SUGGEST_SUCCESS,
-        payload: {
-          suggestions,
-        }
-      })
-      .next()
-      .isDone();
+    it('failing steps', () => {
+      const error = 'Fake';
+      const err = new Error(error);
+      const action = { type: actions.LYRICS_GET };
+      const saga = testSaga(fetchLyrics, action);
+  
+      saga
+        .next()
+        .throw(err)
+        .put({
+          type: actions.LYRICS_GET_FAILED,
+          payload: err.toString(),
+        })
+        .next()
+        .isDone();
+    });
+
   });
+  
+  describe('fetchSuggestions', () => {
+    
+    it('successful steps', () => {
+      const query = 'Query';
+      const suggestions = [];
+  
+      const saga = testSaga(fetchSuggestions, {
+        type: actions.LYRICS_SUGGEST, 
+        payload: query,
+      });
+  
+      saga
+        .next()
+        .call(service.suggest, query)
+        .next({
+          suggestions,
+        })
+        .put({
+          type: actions.LYRICS_SUGGEST_SUCCESS,
+          payload: {
+            suggestions,
+          }
+        })
+        .next()
+        .isDone();
+    });
+  
+    it('failing steps', () => {
+      const error = 'Fake';
+      const err = new Error(error);
+      const action = { type: actions.LYRICS_SUGGEST };
+      const saga = testSaga(fetchSuggestions, action);
+  
+      saga
+        .next()
+        .throw(err)
+        .put({
+          type: actions.LYRICS_SUGGEST_FAILED,
+          payload: err.toString(),
+        })
+        .next()
+        .isDone();
+    });
 
-  it('fetchSuggestions error handling', () => {
-    const error = 'Fake';
-    const action = { type: actions.LYRICS_SUGGEST };
-    const saga = testSaga(fetchSuggestions, action);
-
-    saga
-      .next()
-      .throw(new Error(error))
-      .put({
-        type: actions.LYRICS_SUGGEST_FAILED,
-        payload: `Error: ${error}`,
-      })
-      .next()
-      .isDone();
   });
   
 });
